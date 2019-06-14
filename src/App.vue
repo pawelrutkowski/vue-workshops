d389149f387580c8575ac7f29bb20d4f13a787ee
pawelrutkowski <pawel.rutkowski@htdevelopers.com>
2019-06-14 10:11:24 +0200

    added html sketch

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,26 @@
 <template>
+  <div>
+    <h1>Moje zadania</h1>
+    <input placeholder="Wpisz treść zadania...">
+    <button>Dodaj zadanie</button>
+    <ul>
+      <li>Zadanie1</li>
+      <li>Zadanie2</li>
+      <li>Zadanie3</li>
+    </ul>
+  </div>
 </template>
-
 <script>
 </script>
 
 <style>
+button {
+  border: 1px solid black;
+  padding: 2px;
+}
+input {
+  outline: 0;
+  border-bottom: 1px solid black;
+  margin-right: 10px;
+}
 </style>
